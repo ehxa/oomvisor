9380cc068795fff3401dabe9d098efc9eceba51a
ehxa <2042918@student.uma.pt>
2024-11-25 16:59:14 +0000

    Test

---
+++ frontend/src/app/map/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Map() {
+  return (
+    <div className="bg-blue-500 text-white p-6">
+      <h1 className=" text-3xl font-sans font-bold text-center">OOM Map</h1>
+    </div>
+  );
+}
