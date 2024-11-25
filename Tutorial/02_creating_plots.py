4f2930c9a377edfa3697d559dcee933b771d272c
ehxa <2042918@student.uma.pt>
2024-11-25 14:40:21 +0000

    Organization

--- Tutorial/02_creating_plots.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import xarray as xr
-
-file = 'wrf_2km_mad_fcst_20230729.nc'
-xrds = xr.open_dataset(file)
-#xrds['SST'].plot()
-#xrds['SST'].plot.hist()
-
-plt.show()
--- Tutorial/02_maps.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import xarray as xr
-
-file = 'wrf_2km_mad_fcst_20230729.nc'
-xrds = xr.open_dataset(file)
-
-desired_date = '2023-07-30'
-data_for_desired_date = xrds.sel(Time=desired_date)
-print(data_for_desired_date)
