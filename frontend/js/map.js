6df6facba6c165c6557dcc746bd5378badbe1aa1
ehxa <2042918@student.uma.pt>
2024-12-06 09:10:59 +0000

    Wind layer test

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -3,3 +3,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+L.tileLayer.wms('http://localhost:8080/geoserver/oomvisor/wms', {
+    layers: 'u_wind',
+    format: 'image/png',
+    transparent: true,
+    opacity: 0.2,
+    attribution: 'GeoServer'
+}).addTo(map);
