38ca1451f8a893207c663028faaf43841eaca821
ehxa <2042918@student.uma.pt>
2025-03-17 15:47:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -51,5 +51,79 @@ def get_t2(nc_date, time):
     data = {"lat": latitudes.tolist(),"lon": longitudes.tolist(), "temp": t2_values.tolist()}
     return jsonify(data)
 
+@app.route('/rainc/<string:nc_date>', methods=['GET'])
+def get_rainc(nc_date):
+    url = url_base + nc_date + ".nc"
+    print(url)
+    dataset = xr.open_dataset(url)
+    print(dataset['RAINC'].attrs) 
+    rainc = dataset['RAINC'] 
+    latitudes = dataset['XLAT'].values[0]
+    longitudes = dataset['XLONG'].values[0]
+    times = dataset['Times'].values
+    for i in range(len(times)):
+        rainc_values = rainc[i, :, :].values
+    data = {"lat": latitudes.tolist(),"lon": longitudes.tolist(), "rainc": rainc_values.tolist()}
+    print(data["rainc"][24])
+    return jsonify(data)
+
+@app.route('/rainnc/<string:nc_date>', methods=['GET'])
+def get_rainnc(nc_date):
+    url = url_base + nc_date + ".nc"
+    print(url)
+    dataset = xr.open_dataset(url)
+    print(dataset['RAINNC'].attrs) 
+    rainnc = dataset['RAINNC'] 
+    latitudes = dataset['XLAT'].values[0]
+    longitudes = dataset['XLONG'].values[0]
+    times = dataset['Times'].values
+    rainnc_values = rainnc[0, :, :].values
+    data = {"lat": latitudes.tolist(),"lon": longitudes.tolist(), "rainnc": rainnc_values.tolist()}
+    return jsonify(data)
+
+@app.route('/rainsh/<string:nc_date>', methods=['GET'])
+def get_rainsh(nc_date):
+    url = url_base + nc_date + ".nc"
+    print(url)
+    dataset = xr.open_dataset(url)
+    print(dataset['RAINSH'].attrs) 
+    rainsh = dataset['RAINSH'] 
+    latitudes = dataset['XLAT'].values[0]
+    longitudes = dataset['XLONG'].values[0]
+    times = dataset['Times'].values
+    rainsh_values = rainsh[0, :, :].values
+    data = {"lat": latitudes.tolist(),"lon": longitudes.tolist(), "rainsh": rainsh_values.tolist()}
+    return jsonify(data)
+
+@app.route('/u/<string:nc_date>', methods=['GET'])
+def get_u(nc_date):
+    url = url_base + nc_date + ".nc"
+    print(url)
+    dataset = xr.open_dataset(url)
+    print(dataset['U'].attrs) 
+    u = dataset['U'] 
+    latitudes = dataset['XLAT'].values[0]
+    longitudes = dataset['XLONG'].values[0]
+    times = dataset['Times'].values
+    u_values = u[0, :, :].values
+    data = {"lat": latitudes.tolist(),"lon": longitudes.tolist(), "u": u_values.tolist()}
+    return jsonify(data)
+
+@app.route('/v/<string:nc_date>', methods=['GET'])
+def get_v(nc_date):
+    url = url_base + nc_date + ".nc"
+    print(url)
+    dataset = xr.open_dataset(url)
+    print(dataset['V'].attrs) 
+    v = dataset['V'] 
+    latitudes = dataset['XLAT'].values[0]
+    longitudes = dataset['XLONG'].values[0]
+    times = dataset['Times'].values
+    v_values = v[0, :, :].values
+    data = {"lat": latitudes.tolist(),"lon": longitudes.tolist(), "v": v_values.tolist()}
+    return jsonify(data)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
