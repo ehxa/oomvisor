8ebcaaa7feab074ed8b8d7e82a59b526324d7e2a
ehxa <2042918@student.uma.pt>
2025-05-08 14:29:01 +0100

    Live rain

--- static/js/map.js
+++ static/js/map.js
@@ -14,49 +14,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     minZoom: 9,
 }).addTo(map);
 
-var velocityLayer = L.velocityLayer({
-    displayValues: true,
-    displayOptions: {
-      // label prefix
-      velocityType: "Global Wind",
-  
-      // leaflet control position
-      position: "bottomleft",
-  
-      // no data at cursor
-      emptyString: "No velocity data",
-  
-      // see explanation below
-      angleConvention: "bearingCW",
-  
-      // display cardinal direction alongside degrees
-      showCardinal: false,
-  
-      // one of: ['ms', 'k/h', 'mph', 'kt']
-      speedUnit: "ms",
-  
-      // direction label prefix
-      directionString: "Direction",
-  
-      // speed label prefix
-      speedString: "Speed",
-    },
-    data: data, // see demo/*.json, or wind-js-server for example data service
-  
-    // OPTIONAL
-    minVelocity: 0, // used to align color scale
-    maxVelocity: 10, // used to align color scale
-    velocityScale: 0.005, // modifier for particle animations, arbitrarily defaults to 0.005
-    colorScale: [], // define your own array of hex/rgb colors
-    onAdd: null, // callback function
-    onRemove: null, // callback function
-    opacity: 0.97, // layer opacity, default 0.97
-  
-    // optional pane to add the layer, will be created if doesn't exist
-    // leaflet v1+ only (falls back to overlayPane for < v1)
-    paneName: "overlayPane",
-  });
-
 //------------ General -----------------
 
 async function getDayDataAsync(variable, date) {
@@ -72,6 +29,9 @@ async function getDayDataAsync(variable, date) {
 async function loadDayData() {
     t2DayData = await getDayDataAsync('T2', selectedDate);
     updateHeatmap(0);
+    uDayData = await getDayDataAsync('u', selectedDate);
+    vDayData = await getDayDataAsync('v', selectedDate);
+
 }
 
 //------------ Heat -----------------
